1bacc3eb9ad7f57f51dcab4694c9b20875c145c0
linntrucnguyen <linn.nguyen@loopacademy.no>
2026-02-17 12:46:35 +0100

    feat: add login component with user input

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {MatSelectModule} from '@angular/material/select';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import { LibraryStore } from '../../api/library-store';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MatFormFieldModule, MatInputModule, MatSelectModule, FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private store = inject(LibraryStore);
 
+  name = '';
+  users = this.store.users;
+  loading = this.store.usersLoading;
+  error = this.store.usersError;
+
+  ngOnInit() {
+    this.store.loadUsers();
+  }
+
+  onSubmit() {
+    const trimmed = this.name.trim();
+  
+    this.store.addUser({username: trimmed, collection: []});
+    console.log('User submitted:', trimmed);
+
+    this.name = '';
+  }
 }
