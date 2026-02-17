fed9a49e9fcfaf271b3bef94928abede15ab19b8
MartinHatlem <martinfhatlem@gmail.com>
2026-02-17 11:22:05 +0100

    feat: add routing and navbar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Login } from './pages/login/login';
-import { BookCatalogue } from './pages/book-catalogue/book-catalogue';
-import { BookDetails } from './pages/book-details/book-details';
-import { ReadingList } from './pages/reading-list/reading-list';
+import { Navbar } from './components/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login, BookCatalogue, BookDetails, ReadingList],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
