cb1120319e9049b3e73bef6c1b6ae16a33d32a70
linntrucnguyen <linn.nguyen@loopacademy.no>
2026-02-17 12:45:44 +0100

    feat: add apikey

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://frontend-assignment-api-sgtk.onrender.com'
+  apiUrl: 'https://frontend-assignment-api-sgtk.onrender.com',
+  apiKey: 'void-academy-api-key-123'
 };
