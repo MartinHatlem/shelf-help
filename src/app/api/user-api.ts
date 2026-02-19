479c44b8e34fd26d5e275aed513a8c61c92e8a3d
linntngloop <linn.nguyen@loopacademy.no>
2026-02-19 13:56:53 +0100

    FEAT: add addBookToUserCollection and remove in user api

--- src/app/api/user-api.ts
+++ src/app/api/user-api.ts
@@ -1,13 +1,13 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, switchMap, of } from 'rxjs';
 import { environment } from '../environments/environment';
 
 
 export interface User {
   id: number;
   username: string;
-  collection?: string[];
+  collection?: number[];
 }
 
 export type CreateUser = Omit<User, 'id'>;
@@ -28,8 +28,39 @@ export class UserApi {
     const headers = { 'Content-Type': 'application/json', 'x-api-key': this.apiKey };
     return this.http.post<User>(this.userURL, user, { headers });
   }
-
-  loadUserById(id: number): Observable<User> {
+   loadUserById(id: number): Observable<User> {
     return this.http.get<User>(`${this.userURL}/${id}`);
+  }
+
+  addBookToUserCollection(userId: number, bookId: number): Observable<User> {
+    const headers = { 'Content-Type': 'application/json', 'x-api-key': this.apiKey };
+
+    return this.loadUserById(userId).pipe(
+      switchMap(user => {
+        if (user.collection?.includes(bookId)) {
+          return of(user);
+        }
+
+        const updatedCollection = [...(user.collection || []), bookId];
+        console.log("Collection after adding book:", updatedCollection);
+        return this.http.patch<User>(`${this.userURL}/${userId}`, { collection: updatedCollection }, { headers });
+      })
+    );
+  }
+
+
+  removeBookFromUserCollection(userId: number, bookId: number): Observable<User> {
+    const headers = { 'Content-Type': 'application/json', 'x-api-key': this.apiKey }; 
+
+    return this.loadUserById(userId).pipe(
+      switchMap(user => {
+        if (!user.collection?.includes(bookId)) {
+          return of(user);
+        }
+        const updatedCollection = user.collection.filter(id => id !== bookId);
+        console.log("Collection after removing book:", updatedCollection);
+        return this.http.patch<User>(`${this.userURL}/${userId}`, { collection: updatedCollection }, { headers });
+      })
+    );
   }
 }
