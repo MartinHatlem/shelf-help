c68dc0152a4829a5b65c8666971bc258f228f34e
linntngloop <linn.nguyen@loopacademy.no>
2026-02-19 13:54:33 +0100

    FEAT: import BookCatalgoue

--- src/app/pages/reading-list/reading-list.ts
+++ src/app/pages/reading-list/reading-list.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { BookCard } from '../../components/book-card/book-card';
+import { BookCatalogue } from '../book-catalogue/book-catalogue';
 
 @Component({
   selector: 'app-reading-list',
-  imports: [BookCard],
+  imports: [BookCard, BookCatalogue],
   templateUrl: './reading-list.html',
   styleUrl: './reading-list.css',
 })
 export class ReadingList {
+  
 
 }
