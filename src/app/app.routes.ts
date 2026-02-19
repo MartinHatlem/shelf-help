22c3049e4e5828c37bed46374724d331620444c0
MartinHatlem <martinfhatlem@gmail.com>
2026-02-19 10:29:26 +0100

    feat: add auth guard and deactivate navbar options

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,11 @@ import { Login } from './pages/login/login';
 import { BookCatalogue } from './pages/book-catalogue/book-catalogue';
 import { BookDetails } from './pages/book-details/book-details';
 import { ReadingList } from './pages/reading-list/reading-list';
+import { authGuard } from './guards/auth-guard/auth-guard';
 
 export const routes: Routes = [
   { path: '', component: BookCatalogue },
   { path: 'login', component: Login },
   { path: 'book-details/:id', component: BookDetails },
-  { path: 'reading-list', component: ReadingList },
+  { path: 'reading-list', component: ReadingList, canActivate: [authGuard] },
 ];
