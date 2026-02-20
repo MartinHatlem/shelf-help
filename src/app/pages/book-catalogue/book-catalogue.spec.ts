5fc95b688a42bf46c1031ff0e9a9094c464926ca
MartinHatlem <martinfhatlem@gmail.com>
2026-02-20 12:48:59 +0100

    test: add mocklibrarystore to catalogue

--- src/app/pages/book-catalogue/book-catalogue.spec.ts
+++ src/app/pages/book-catalogue/book-catalogue.spec.ts
@@ -1,6 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { BookCatalogue } from './book-catalogue';
+import { LibraryStore } from '../../api/library-store';
+
+class MockLibraryStore {
+  books() {
+    return [{ title: 'Test Book', author: 'Test Author' }];
+  }
+  currentUser() {
+    return { name: 'Test User' };
+  }
+}
 
 describe('BookCatalogue', () => {
   let component: BookCatalogue;
@@ -8,7 +19,10 @@ describe('BookCatalogue', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [BookCatalogue]
+      imports: [BookCatalogue, RouterTestingModule],
+      providers: [
+        { provide: LibraryStore, useClass: MockLibraryStore }
+      ]
     })
     .compileComponents();
 
@@ -20,4 +34,16 @@ describe('BookCatalogue', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a book grid container', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector('.book-grid')).toBeTruthy();
+  });
+
+  it('should load book card components', async () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    await fixture.whenStable();
+    expect(compiled.querySelector('app-book-card')).toBeTruthy();
+  });
+
 });
