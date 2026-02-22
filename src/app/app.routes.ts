00a5a5a2cc42a88d24dc9cf6365b164ae49e5610
linntngloop <linn.nguyen@loopacademy.no>
2026-02-22 19:25:44 +0100

    feat: add routing to bookform

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { BookCatalogue } from './pages/book-catalogue/book-catalogue';
 import { BookDetails } from './pages/book-details/book-details';
 import { ReadingList } from './pages/reading-list/reading-list';
 import { authGuard } from './guards/auth-guard/auth-guard';
+import { BookForm } from './components/book-form/book-form';
 
 export const routes: Routes = [
   { path: '', component: BookCatalogue },
   { path: 'login', component: Login },
   { path: 'book-details/:id', component: BookDetails },
   { path: 'reading-list', component: ReadingList, canActivate: [authGuard] },
+  { path: 'add-book', component: BookForm, canActivate: [authGuard] },
 ];
